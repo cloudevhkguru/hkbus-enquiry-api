9434921d6727c205fc74b6fb486955816d6bc316
Cavour Poon <cavouriypoon@outlook.com>
2022-01-12 01:04:09 +0800

    Change all responses of KMB and CTBNWFB extends base response

--- src/main/java/org/cloudevguru/hkbus/enquiry/api/dto/ctbnwfb/v1/CTBNWFBv1RouteListResponse.java
+++ src/main/java/org/cloudevguru/hkbus/enquiry/api/dto/ctbnwfb/v1/CTBNWFBv1RouteListResponse.java
@@ -4,43 +4,10 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class CTBNWFBv1RouteListResponse {
-	
-	@JsonProperty("type")
-	private String type;
-    
-	@JsonProperty("version")
-	private String version;
-
-	@JsonProperty("generated_timestamp")
-    private String generatedTimestamp;
+public class CTBNWFBv1RouteListResponse extends CTBNWFBv1BaseResponse{
 	
 	@JsonProperty("data")
 	private List<CTBNWFBv1RouteDto> dtos;
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(String version) {
-		this.version = version;
-	}
-
-	public String getGeneratedTimestamp() {
-		return generatedTimestamp;
-	}
-
-	public void setGeneratedTimestamp(String generatedTimestamp) {
-		this.generatedTimestamp = generatedTimestamp;
-	}
 
 	public List<CTBNWFBv1RouteDto> getDtos() {
 		return dtos;
--- src/main/java/org/cloudevguru/hkbus/enquiry/api/dto/kmb/v1/KMBv1RouteListResponse.java
+++ src/main/java/org/cloudevguru/hkbus/enquiry/api/dto/kmb/v1/KMBv1RouteListResponse.java
@@ -4,43 +4,10 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class KMBv1RouteListResponse {
-	
-	@JsonProperty("type")
-	private String type;
-    
-	@JsonProperty("version")
-	private String version;
+public class KMBv1RouteListResponse extends KMBv1BaseResponse {
 
-	@JsonProperty("generated_timestamp")
-    private String generatedTimestamp;
-	
 	@JsonProperty("data")
 	private List<KMBv1RouteDto> dtos;
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(String version) {
-		this.version = version;
-	}
-
-	public String getGeneratedTimestamp() {
-		return generatedTimestamp;
-	}
-
-	public void setGeneratedTimestamp(String generatedTimestamp) {
-		this.generatedTimestamp = generatedTimestamp;
-	}
 
 	public List<KMBv1RouteDto> getDtos() {
 		return dtos;
@@ -49,5 +16,5 @@ public class KMBv1RouteListResponse {
 	public void setDtos(List<KMBv1RouteDto> dtos) {
 		this.dtos = dtos;
 	}
-	
+
 }
