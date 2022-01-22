3a8dd0f1d25697eac3ccb6991dfd9efcfd649573
Cavour Poon <cavouriypoon@outlook.com>
2022-01-22 11:24:45 +0800

    Add service serach route by route with like clause

--- src/main/java/org/cloudevguru/hkbus/enquiry/api/manager/KMBManager.java
+++ src/main/java/org/cloudevguru/hkbus/enquiry/api/manager/KMBManager.java
@@ -31,13 +31,13 @@ public class KMBManager {
 		return kmbService.getKMBv1RouteList();
 	}
 
-	public KMBv1RouteListResponse getKMBv1RouteListByRoute(String route) {
+	public KMBv1RouteListResponse getKMBv1RouteListByRouteAndServiceType(String route,String serviceType) {
 		KMBv1RouteResponse kmbResponse = new KMBv1RouteResponse();
 		KMBv1RouteListResponse response = new KMBv1RouteListResponse();
 		List<KMBv1RouteDto> dtos = new ArrayList<KMBv1RouteDto>();
 		for (DirectionFullEum directionFull : DirectionFullEum.values()) {
 			kmbResponse = kmbService.getKMBv1RouteByRouteAndDirectionAndServiceType(route.toUpperCase(),
-					directionFull.getValue(), "1");
+					directionFull.getValue(), serviceType);
 			if (kmbResponse.getDto().getOriginEn() != null) {
 				dtos.add(kmbResponse.getDto());
 			}
@@ -49,9 +49,9 @@ public class KMBManager {
 		return response;
 	}
 
-	public KMBv1RouteResponse getKMBv1RouteByRouteAndDirection(String route, String direction) {
+	public KMBv1RouteResponse getKMBv1RouteByRouteAndDirectionAndServiceType(String route, String direction,String serviceType) {
 		String directionFull = utilityService.convertDirectionToFull(direction);
-		return kmbService.getKMBv1RouteByRouteAndDirectionAndServiceType(route.toUpperCase(), directionFull, "1");
+		return kmbService.getKMBv1RouteByRouteAndDirectionAndServiceType(route.toUpperCase(), directionFull, serviceType);
 	}
 
 	// stop
@@ -68,14 +68,14 @@ public class KMBManager {
 		return kmbService.getKMBv1RouteStopList();
 	}
 
-	public KMBv1RouteStopListResponse getKMBv1RouteStopListByRouteAndDirection(String route, String direction) {
+	public KMBv1RouteStopListResponse getKMBv1RouteStopListByRouteAndDirectionAndServiceType(String route, String direction,String serviceType) {
 		String directionFull = utilityService.convertDirectionToFull(direction);
 		return kmbService.getKMBv1RouteStopListByRouteAndDirectionAndServiceType(route.toUpperCase(), directionFull,
-				"1");
+				serviceType);
 	}
 
-	public KMBv1RouteStopEtaResponse getKMBv1RouteStopEtaByRouteAndStopId(String stopId, String route) {
-		return kmbService.getKMBv1RouteStopEtaByStopIdAndRouteAndServiceType(stopId, route.toUpperCase(), "1");
+	public KMBv1RouteStopEtaResponse getKMBv1RouteStopEtaByRouteAndStopIdAndServiceType(String stopId, String route,String serviceType) {
+		return kmbService.getKMBv1RouteStopEtaByStopIdAndRouteAndServiceType(stopId, route.toUpperCase(), serviceType);
 	}
 
 }
--- src/test/java/org/cloudevguru/hkbus/enquiry/api/BaseTestEntity.java
+++ src/test/java/org/cloudevguru/hkbus/enquiry/api/BaseTestEntity.java
@@ -2,22 +2,28 @@ package org.cloudevguru.hkbus.enquiry.api;
 
 import java.util.Date;
 
+import org.cloudevguru.hkbus.enquiry.api.manager.SystemManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BaseTestEntity {
-	
+
+	@Autowired
+	private SystemManager systemManager;
+
 	@BeforeEach
 	public void printTestNameBeforeTest(TestInfo testInfo) {
-		System.out.println(String.format("Start %s at %s",testInfo.getDisplayName(),new Date()));
+		System.out.println(String.format("Start %s at %s", testInfo.getDisplayName(), new Date()));
 	}
-	
+
 	@AfterEach
 	public void printTestNameAfterTest(TestInfo testInfo) {
-		System.out.println(String.format("Completed %s at %s",testInfo.getDisplayName(),new Date()));
+		systemManager.cacheAllEvict();
+		System.out.println(String.format("Completed %s at %s", testInfo.getDisplayName(), new Date()));
 	}
-	
+
 }
